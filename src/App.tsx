aa8af07c4133ecbd0ae094db85371ec74325a3e4
liuyao <1457569132@qq.com>
2022-10-06 18:02:56 +0800

    feat:搭建初始化路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.scss'
+import Layout from './pages/Layout'
+import Login from './pages/Login'
 const App = () => {
-  return <div className="app">计科院</div>
+  return (
+    <Router>
+      <Switch>
+        <Route path={'/home'} component={Layout}></Route>
+        <Route path={'/login'} component={Login}></Route>
+      </Switch>
+    </Router>
+  )
 }
 export default App
