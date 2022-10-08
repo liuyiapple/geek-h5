e9f4cb2ef510ebc65b0ea98285caa27867d3d8ad
liuyao <1457569132@qq.com>
2022-10-08 23:04:13 +0800

    build: 适配

--- craco.config.js
+++ craco.config.js
@@ -4,8 +4,26 @@ module.exports = {
   webpack: {
     alias: {
       // 约定 使用@ 表示 src 文件路径
-      "@" : path.resolve(__dirname,"src"),
-      "@scss" : path.resolve(__dirname,"src/assets/styles")
+      "@": path.resolve(__dirname, "src"),
+      "@scss": path.resolve(__dirname, "src/assets/styles")
     }
-  }
+  },
+  style: {
+    postcss: {
+      mode: "extends",
+      loaderOptions: {
+        postcssOptions: {
+          ident: "postcss",
+          plugins: [
+            [
+              "postcss-px-to-viewport-8-plugin",
+              {
+                viewportWidth: 375, // 设计稿的视口宽度
+              },
+            ],
+          ],
+        },
+      },
+    },
+  },
 }
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,10 @@
 import { Button } from 'antd-mobile'
+import './index.css'
 const Login = () => {
   return (
     <div>
       <Button>登录</Button>
+      <p className="p_item">11111111111111111</p>
     </div>
   )
 }
