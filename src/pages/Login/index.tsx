1c54d4ec834f4d471031da1d9551f86ba679fe62
liuyao <1457569132@qq.com>
2022-10-09 22:02:43 +0800

    feat: 添加字体图标库

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,14 @@ import './index.scss'
 const Login = () => {
   return (
     <div>
-      <Button>登录</Button>
+      <Button>登录
+
+      <svg className="icon" aria-hidden="true">
+        {/* 使用时，只需要将此处的 iconbtn_like_sel 替换为 icon 的名称即可*/}
+        <use xlinkHref="#iconbtn_like_sel"></use>
+      </svg>
+
+      </Button>
       <p className="p_item">11111111111111111</p>
     </div>
   )
